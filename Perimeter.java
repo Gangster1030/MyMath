08632c8a4e7227812a0b2ad38e6387418c1b65e2
Gangster1030 <47955176+Gangster1030@users.noreply.github.com>
2019-04-28 11:56:45 -0700

    Rename Perimeter to Perimeter.java

---
+++ Perimeter.java
@@ -0,0 +1,19 @@
+package MyMath
+public class Perimeter {
+	int TrianglePerimeter(int length1, int length2, int length3) {
+		int result = length1+length2+length3;
+		System.out.println("Perimeter of the triangle is "+result)
+		return result;
+	}
+	int RectanglePerimeter(int length1, int length2, int length3, length4) {
+		int result = length1+length2+length3+length4;
+		System.out.println("Perimeter of the rectangle is "+result)
+		return result;
+		
+	}
+	int SquarePerimeter(int length1) {
+		int result = length1*4;
+		System.out.println("Perimeter of the square is "+result)
+		return result;
+
+}
