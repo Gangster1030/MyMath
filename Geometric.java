1a102f4b24604c2aa00276a115b0a2171e8a363d
Gangster1030 <47955176+Gangster1030@users.noreply.github.com>
2019-04-28 12:05:19 -0700

    Create Geometric.java

---
+++ Geometric.java
@@ -0,0 +1,15 @@
+package MyMath;
+
+public class Geometric {
+
+	public int Multiply(int x, int y) {
+		int z = x * y;
+		System.out.println("Multiplication of " + x + " and " + y + " = " + z);
+		
+		return z;
+	}
+	public int Division(int x, int y) {
+		int z=x/y
+		
+		System.out.println("Division of "+x+" and "+y+" = "+z)
+	}	return z;
