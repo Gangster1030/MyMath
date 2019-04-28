bd84b1e9a4bbd150ba01d7397d95d45afef4c075
Gangster1030 <47955176+Gangster1030@users.noreply.github.com>
2019-04-28 12:03:37 -0700

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+package MyMath;
+
+public class ArithmaticCalc {
+	
+	public  int Addition(int x, int y) {
+		int z = x + y;
+		System.out.println("Sum of " + x + " and " + y + " = " + z);
+		
+		return z;
+	}
+	public int Subtract(int x, int y) {
+		int z=x-y;
+		
+		System.out.print.ln(Diiference of "+x+" and "+y+" = "+z)
+		return z;
+	}
+	
+}
